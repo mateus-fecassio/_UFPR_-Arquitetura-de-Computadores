56cdf33569bca504aab5ffeefcad283067188531
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2019-10-01 09:14:39 -0300

    scripts para automatizar os testes em todos os traços

---
+++ YAGS/execute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./orcs -t ../traces/astar/astar.CINT.PP200M
+
+#./orcs -t ../traces/calculix/calculix.CFP.PP200M > calculix.txt
+
+#./orcs -t ../traces/dealII/dealII.CFP.PP200M > dealII.txt
+
+#./orcs -t ../traces/gromacs/gromacs.CFP.PP200M > gromacs.txt
+
+#./orcs -t ../traces/libquantum/libquantum.CINT.PP200M > libquantum.txt
+
+#./orcs -t ../traces/namd/namd.CFP.PP200M > namd.txt
