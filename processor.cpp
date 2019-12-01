e89408908727f595a6d29ca54659801efecfb89b
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2019-11-30 23:59:32 -0300

    Entrega do segundo trabalho sobre prefetcher de instrucoes

--- processor.cpp
+++ processor.cpp
@@ -7,7 +7,9 @@ processor_t::processor_t() {
 
 // =====================================================================
 void processor_t::allocate() {
-
+	memset (orcs_engine.processor->CL1, 0, sizeof(cache_line_t)*256*4);
+	memset (orcs_engine.processor->CL2, 0, sizeof(cache_line_t)*2048*8);
+	memset (orcs_engine.processor->stride, 0, sizeof(stride_line_t)*16);
 };
 
 // =====================================================================
@@ -22,6 +24,67 @@ void processor_t::clock_STRIDE()
 	}
 	else //caso haja instruções
 	{
+		orcs_engine.processor->cycles ++;
+		if (orcs_engine.processor->latency > 0)
+		{
+			orcs_engine.processor->latency --;
+		}
+		else
+		{
+			if (new_instruction.opcode_operation == INSTRUCTION_OPERATION_MEM_LOAD)
+			{
+				bool cache_L1_hit = 0;
+				bool cache_L2_hit = 0;
+				int j;
+
+				//FAZER A BUSCA NA CACHE L1
+				orcs_engine.processor->latency = 1;
+				uint64_t index = new_instruction.opcode_address & 255;
+				uint64_t tag = new_instruction.opcode_address >> 8;
+
+				for (j = 0; j < 4; ++j)
+				{
+					if (orcs_engine.processor->CL1[index][j].tag == tag)
+						cache_L1_hit = 1;
+				}
+
+				//se não houve hit na cache L1
+				if (!cache_L1_hit)
+				{
+					//FAZER A BUSCA NA CACHE L2
+					orcs_engine.processor->latency += 4;
+					index = new_instruction.opcode_address & 2047;
+					tag = new_instruction.opcode_address >> 11;
+
+					for (j = 0; j < 8; ++j)
+					{
+						if (orcs_engine.processor->CL2[index][j].tag == tag)
+							cache_L2_hit = 1;
+					}
+				}
+
+				//se houver miss nas duas, chamar o STRIDE
+				if (!cache_L1_hit && !cache_L2_hit)
+				{
+					//fazer alguma coisa aqui no stride prefetcher
+					orcs_engine.processor->latency += 200; //VERIFICAR
+				}
+			}
+
+
+
+
+
+
+			else if (new_instruction.opcode_operation == INSTRUCTION_OPERATION_MEM_STORE)
+			{
+				//FAZER A BUSCA NA CACHE L1
+
+				//FAZER A BUSCA NA CACHE L2
+
+				//se houver miss nas duas, chamar o STRIDE
+			}
+		} //end if (orcs_engine.processor->latency > 0)
 
 	} //else caso haja instruções
 };
--- processor.hpp
+++ processor.hpp
@@ -1,25 +1,26 @@
 // ============================================================================
 // ============================================================================
-class CACHE_line_t
+class cache_line_t
 {
   private:
 
   public:
+    uint64_t valid;  //verifica se a linha está suja (0 = não, 1 = sim)
     uint64_t tag;    //guarda o restante do PC
-    uint64_t data;   //dado
+    //uint64_t data; //dado
     uint64_t lru;    //last recently used (mais velho)
 };
 
 
-class STRIDE_line_t
+class stride_line_t
 {
   private:
 
   public:
     uint64_t tag;           //guarda o restante do PC
-    uint64_t last_address;
-    uint64_t stride;
-    //STATUS
+    uint64_t last_address;  //guarda o último endereço de memória associado a aquele PC
+    uint64_t stride;        //guarda o deslocamento calculado
+    uint64_t status;        //confiabilidade do dado (0 = inativo, 1 = treinamento, 2 = ativo)
 };
 
 class processor_t
@@ -27,8 +28,11 @@ class processor_t
     private:
 
     public:
-      cache_line_t CL1[1024][4];
-      cache_line_t CL2[1024][4];
+      cache_line_t CL1[256][4];
+      cache_line_t CL2[2048][8];
+      stride_line_t stride[16];
+      uint64_t latency;
+      uint64_t cycles;
 
 
 		// ====================================================================
